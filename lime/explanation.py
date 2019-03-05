7d6ef1a8d60e7eec972d2feb5cb975c212c355e8
unstoppable-yuvi <37369005+unstoppable-yuvi@users.noreply.github.com>
2019-03-05 17:00:28 +0530

    Update explanation.py

--- lime/explanation.py
+++ lime/explanation.py
@@ -173,10 +173,10 @@ class Explanation(object):
         vals.reverse()
         names.reverse()
         file_ = "C:/Users/a.i.mevawala/Desktop/Hackathon/Explanation/stroke2.txt"
-		for item in names:
-            file_.write("%s\n" % item)
-		for item in vals:
-            file_.write("%s\n" % item)
+	for item in names:
+		file_.write("%s\n" % item)
+	for item in vals:
+		file_.write("%s\n" % item)
         colors = ['green' if x > 0 else 'red' for x in vals]
         pos = np.arange(len(exp)) + .5
         plt.barh(pos, vals, align='center', color=colors)
